2384c4011ac2af227de8c55fc410caad0193b1d8
ZayWill21 <156355996+ZayWill21@users.noreply.github.com>
2024-10-10 18:42:13 -0400

    Welcome page starting file

--- TuneSort/TuneSort/urls.py
+++ TuneSort/TuneSort/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from playlistSorter import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome',views.welcome),
 ]
