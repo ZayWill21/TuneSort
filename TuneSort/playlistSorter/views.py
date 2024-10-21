9f2faaae8a97e77a2b066b9ab3bf26d25781c7a4
ZayWill21 <156355996+ZayWill21@users.noreply.github.com>
2024-10-21 19:41:15 -0400

    Terraform file creation

--- TuneSort/playlistSorter/views.py
+++ TuneSort/playlistSorter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 def welcome(request):
-    return render(request, 'playlistSorter/welcome.html')
+    
+    return render(request, 'playlistSorter/welcome.html', {'today': datetime.today()})
