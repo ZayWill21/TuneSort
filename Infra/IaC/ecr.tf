27ef6910d3d804fa37e52a659b8a012e903aec9d
Isaiah William <156355996+ZayWill21@users.noreply.github.com>
2025-10-24 13:48:20 -0400

    Add ECR repository and associated policies

---
+++ Infra/IaC/ecr.tf
@@ -0,0 +1,65 @@
+# ECR Repository
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.stack_name}-repository"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.stack_name}-ecr-repository"
+    }
+  )
+}
+
+# ECR Repository Policy
+resource "aws_ecr_repository_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowPullFromEKSNodeRole",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.node_instance_role.arn
+        },
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+      }
+    ]
+  })
+}
+
+# ECR Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "any",
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
