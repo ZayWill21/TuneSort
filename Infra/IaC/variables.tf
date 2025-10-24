95ffdad6d73d0eb8ff91e4c5fed2335b013bf410
Isaiah William <156355996+ZayWill21@users.noreply.github.com>
2025-10-24 13:38:08 -0400

    Add VPC CIDR blocks and EKS configuration variables
    Added multiple CIDR block variables for public and private subnets, along with EKS cluster version and node group instance types.

--- Infra/IaC/variables.tf
+++ Infra/IaC/variables.tf
@@ -1,10 +1,55 @@
-variable "env" {
-  description = "Where is this being deployed"
-  default = "dev"
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_cidr_block1" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_cidr_block2" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "public_cidr_block3" {
+  type    = string
+  default = "10.0.3.0/24"
+}
+
+variable "private_cidr_block1" {
+  type    = string
+  default = "10.0.4.0/24"
+}
+
+variable "private_cidr_block2" {
+  type    = string
+  default = "10.0.5.0/24"
+}
+
+variable "private_cidr_block3" {
+  type    = string
+  default = "10.0.6.0/24"
+}
+
+variable "eks_cluster_version" {
+  type = string
+}
+
+variable "node_group_instance_types" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "stack_name" {
+  type    = string
+  default = "eks-cluster"
+  description = "Name to be used on all resources as prefix"
 }
 
 variable "region" {
-  description = "Where the resources are being deployed"
-  default = "us-east-1"
+  type    = string
+  default = "us-west-2"
+  description = "AWS region"
 }
-
